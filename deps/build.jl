e6945f9e6ed32f861fd776466f7310e62508b929
Alexander Barth <barth.alexander@gmail.com>
2017-11-21 12:34:26 +0100

    try scitools

--- deps/build.jl
+++ deps/build.jl
@@ -5,7 +5,7 @@ using Conda
 @BinDeps.setup
 libudunits2 = library_dependency("libudunits2", aliases = ["udunits2","udunits"])
 
-#Conda.add_channel("conda-forge")
+Conda.add_channel("scitools")
 provides(Conda.Manager, "udunits2", libudunits2)
 provides(AptGet, "libudunits2-dev", libudunits2, os = :Linux)
 provides(Yum, "udunits2-devel", libudunits2, os = :Linux)
